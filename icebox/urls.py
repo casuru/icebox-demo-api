7ff7702c2065fac79ff40594feafaed7428a32eb
Chineye Asuru <causu@uab.edu>
2019-11-01 08:42:05 -0500

    UserViewSet and Token authentication

--- icebox/urls.py
+++ icebox/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken import views
+from rest_framework import routers
+from icebox.views import UserViewSet
+
+router = routers.SimpleRouter()
+
+
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('v1/auth/token/', views.obtain_auth_token),
+    path('v1/', include(router.urls))
 ]
---
+++ icebox/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from django.contrib.auth import get_user_model
+from icebox.serializers import UserSerializer
+
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
